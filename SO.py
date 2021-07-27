13c046b0cbab0b36590f9dc0dde9df133559e6e9
Sam Salemi <40397217+samsalemi@users.noreply.github.com>
2021-07-27 13:32:42 -0400

    Update SO.py

--- SO.py
+++ SO.py
@@ -1,6 +1,6 @@
 # June 19 2018
 # Author: Samuel Salemi
-# University of Guelph Masters Candidate
+# University of Guelph Masters Graduate
 # The script runs Static Optimization trials for the Gait2354 Model.
 
 
